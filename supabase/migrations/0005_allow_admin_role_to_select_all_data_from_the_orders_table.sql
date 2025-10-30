407d706379aa022a04480000ec42bffb4a7d5f30
[dyad] <git@dyad.sh>
2025-10-30 11:11:55 +0500

    Reverted all changes back to version 747d59c77f939c1e690ab1ba4b5b31daae59aa00

--- supabase/migrations/0005_allow_admin_role_to_select_all_data_from_the_orders_table.sql
+++
@@ -1,2 +0,0 @@
-CREATE POLICY "Admins can view all orders" ON public.orders 
-FOR SELECT TO authenticated USING (auth.uid() IN ( SELECT profiles.id FROM profiles WHERE (profiles.role = 'admin'::text)));
--- supabase/migrations/0008_allow_admin_role_to_select_all_profiles_users_.sql
+++
@@ -1,2 +0,0 @@
-CREATE POLICY "Admins can view all profiles" ON public.profiles 
-FOR SELECT TO authenticated USING (auth.uid() IN ( SELECT profiles.id FROM profiles WHERE (profiles.role = 'admin'::text)));
