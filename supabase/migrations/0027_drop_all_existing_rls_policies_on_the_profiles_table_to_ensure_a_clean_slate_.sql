b0ac9ab7d0eee283de58bee77780c5915399469f
babarazaminfodev-debug <babarazam.info.dev@gmail.com>
2025-10-31 15:44:21 +0500

    [dyad] wrote 1 file(s), executed 2 SQL queries

---
+++ supabase/migrations/0027_drop_all_existing_rls_policies_on_the_profiles_table_to_ensure_a_clean_slate_.sql
@@ -0,0 +1,5 @@
+DROP POLICY IF EXISTS "profiles_select_policy" ON public.profiles;
+DROP POLICY IF EXISTS "profiles_insert_policy" ON public.profiles;
+DROP POLICY IF EXISTS "profiles_update_policy" ON public.profiles;
+DROP POLICY IF EXISTS "profiles_delete_policy" ON public.profiles;
+DROP POLICY IF EXISTS "Admins can read all profiles non-recursive" ON public.profiles;
